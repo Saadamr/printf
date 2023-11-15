3cbfa7d8bc314a6c1ca94eaa24501035337dd2f7
Saadamr <s.amroui@mundiapolis.ma>
2023-11-14 17:10:56 -0800

    1

--- printf.c
+++ printf.c
@@ -12,24 +12,37 @@ int _printf(const char *format, ...) {
         if (*format == '%') {
             format++; /* Move to the next character after % */
 
-            if (*format == 'd' || *format == 'i') {
-                int num = va_arg(args, int);
-                printf("%d", num);
-                count += snprintf(NULL, 0, "%d", num);
-            } else {
-                putchar('%');
-                putchar(*format);
-                count += 2; /* Increment count for '%' and the character after '%' */
+            switch (*format) {
+                case 'c': {
+                    char ch = (char)va_arg(args, int);
+                    putchar(ch);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *str = va_arg(args, char *);
+                    while (*str != '\0') {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    break;
             }
         } else {
             putchar(*format);
             count++;
         }
 
-        format++; /* Move to the next character in the format string */
+        format++;
     }
 
     va_end(args);
     return count;
 }
-
